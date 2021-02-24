ca4a7a814bf801faf251dc76df745f73159c8fae
haocongshun <haocongshun@hotmail.com>
2021-02-24 14:58:41 +0800

    Delete the front-end code

--- biu-infinity-ui/src/views/overview/monitoring/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <d2-container>
-    <template slot="header">header</template>
-    overview
-    <template slot="footer">footer</template>
-  </d2-container>
-</template>
--- biu-infinity-ui/src/views/system/index/page.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <d2-container>
-    <template slot="header">header</template>
-    biu-infinity 大数据平台
-    <template slot="footer">footer</template>
-  </d2-container>
-</template>
