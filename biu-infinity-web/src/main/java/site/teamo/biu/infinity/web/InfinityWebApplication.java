8b233d216aae3f19dddca124f4c85d53d8068bd1
haocongshun <haocongshun@hotmail.com>
2021-02-25 21:56:33 +0800

    Enable scheduling for infinity-web

--- biu-infinity-web/src/main/java/site/teamo/biu/infinity/web/InfinityWebApplication.java
+++ biu-infinity-web/src/main/java/site/teamo/biu/infinity/web/InfinityWebApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -15,6 +16,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = {"site.teamo.biu.infinity.web.dao"})
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableScheduling
 public class InfinityWebApplication {
 
     public static void main(String[] args) {
