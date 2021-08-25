b7286eafd23a1cbd4d3406f8eaec13e18e605140
haocongshun <haocongshun@hotmail.com>
2021-08-25 15:58:26 +0800

    [feature]Add data source manager

--- data-source-manager/biu-infinity-data-source-manager/src/main/java/site/teamo/biu/infinity/data/source/manager/controller/DataSourceController.java
+++ data-source-manager/biu-infinity-data-source-manager/src/main/java/site/teamo/biu/infinity/data/source/manager/controller/DataSourceController.java
@@ -2,7 +2,7 @@ package site.teamo.biu.infinity.data.source.manager.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import site.teamo.biu.infinity.fweb.common.util.BiuJSONResult;
+import site.teamo.biu.infinity.fapi.common.BiuJSONResult;
 
 /**
  * @author haocongshun
